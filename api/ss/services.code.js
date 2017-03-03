9428eea0a9f93df9bccdd8e9e390d90911adc048
邱俊 <i@qiujun.me>
2017-03-03 16:57:12 +0800

    feat:配合前端调整接口

--- api/ss/services.code.js
+++ api/ss/services.code.js
@@ -1,13 +1,12 @@
 'use strict';
 
 const ss = require('../../service/ss');
-const format = require('../../lib/format').ss;
 
 module.exports = {
   *get(req, res) {
     let user_id = req.session.ss.user.user_id;
     let service = yield ss.findServiceAsync(user_id);
 
-    res.json(format.service(service));
+    res.json(service);
   },
 };
