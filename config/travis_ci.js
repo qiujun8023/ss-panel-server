f54de82143ef4f0f53d5fa2ac8537d27b39cd2d9
邱俊 <i@qiujun.me>
2017-03-20 21:15:52 +0800

    feat: gitlab-ci 迁移到 travis-ci

---
+++ config/travis_ci.js
@@ -0,0 +1,31 @@
+'use strict';
+
+let config = {
+  env: 'test',
+  debug: false,
+
+  redis: {
+    session: {
+      host: 'redis',
+      port: 6379,
+      keyPrefix: 'shard:session:',
+    },
+    wechat: {
+      host: 'redis',
+      port: 6379,
+      keyPrefix: 'shard:wechat:',
+    },
+  },
+
+  mysql: {
+    shard: {
+      poolSize: 5,
+      host: 'mysql',
+      user: 'root',
+      password: '',
+      database: 'shard',
+    },
+  },
+};
+
+module.exports = config;
